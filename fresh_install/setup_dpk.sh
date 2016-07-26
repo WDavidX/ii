338c2976137141b2d78e48aafe76c928302201f7
David Xu <mysolowing@gmail.com>
2016-07-26 17:50:12 -0500

    added pip requirements

--- fresh_install/setup_dpk.sh
+++ fresh_install/setup_dpk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -e "\nInstallation Starts"
-echo $pwd
+INIT_SETUP_DIR=$(pwd)
+echo -e "\nInstallation starts $(pwd)"
 ############################################################
 ########## apt-get updates and preparation
 ############################################################
 sudo dpkg --configure -a # consolidate previous installs
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get dist-upgrade
 
 ############################################################
 ########## Essentials untilities
@@ -20,8 +20,11 @@ sudo apt-get install -y bash-completion
 ############################################################
 ########## Untilities
 ############################################################
+sudo scons libncurses5-dev pps-tools
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev # for SSL
+sudo apt-get install -y openssh-client openssh-server
 sudo apt-get install -y git 
-sudo apt-get install -y motion
+
 
 ############################################################
 ########## Compilers and program languages
@@ -37,9 +40,10 @@ sudo apt-get install -y clang
 
 # Python
 sudo apt-get install -y python python-dev python-pip python-pandas python-smbus spyder  pep8
+sudo apt-get install -y python3-pip
 
 # Java
-sudo apt-get install -y openjdk-7-jdk -y
+sudo apt-get install -y openjdk-7-jdk 
 
 # C sharp
 sudo apt-get install -y  mono-complete
@@ -58,8 +62,23 @@ sudo apt-get install -y iselect screenie   tcsh   htop   nload  iftop iptraf net
 # X related dependencies
 sudo apt-get install -y xorg-dev libpng12-dev libjpeg8-dev libgif-dev libtiff5 libtiff5-dev  libncurses5-dev
 
+# Other
+sudo apt-get install -y motion # webcam integration
+sudo apt-get install gpsd gpsd-clients python-gps # gps receiver
+
 # texlive
 # sudo apt-get install texlive-xetex latexmk -y 
 #sudo apt-get install motion iselect screenie ntpdate python python-dev python-pip python-pandas openjdk-7-jdk tightvncserver  git  emacs tcsh screen vim emacs tcsh g++ nano i2c-tools python-smbus libi2c-dev samba-common eclipse htop screen spyder nload  iftop iptraf nethogs  bmon slurm tcptrack bwm-ng cbm  lsof collectl mono-complete inkscape latexmk bashdb vim pep8 texlive-xetex samba -y
 
 sudo apt-get upgrade -y
+
+############################################################
+########## Python packages
+############################################################
+
+sudo python -m pip install -U pip
+sudo python3 -m pip3 install -U pip3
+sudo pip3 install numpy
+sudo pip install numpy
+sudo pip install -r pip_requirements.txt
+sudo pip3 install -r pip_requirements.txt
